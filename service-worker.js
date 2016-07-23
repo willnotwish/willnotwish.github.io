b95818e214542655db4c12d962a8e3bdea284050
Nick <nadams@dsc.net>
2016-07-23 15:54:56 +0100

    Starting with service workers

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+self.addEventListener('fetch', function(event) {
+  console.log('got a request');
+
+  var salutation = 'Hello, ';
+  var whom = decodeURIComponent(event.request.url.match(/\/([^/]*)$/)[1]);
+  var energyLevel = (whom === 'Cleveland') ? '!!!' : '!';
+  var version = '\n\n(Version 1)';
+
+  var body = new Blob([salutation, whom, energyLevel, version]);
+
+  event.respondWith(new Response(body));
+});
